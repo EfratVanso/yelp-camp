c8d456c6dd5fb81f9878912f25ab34be166f908b
efiVanso <efi.vanso@gmail.com>
2020-08-14 02:54:15 +0300

    adding register form & routs

--- app.js
+++ app.js
@@ -32,6 +32,9 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+//=============
+//  ROUTES
+//=============
 app.get("/", function (req, res) {
   res.render("landing");
 });
@@ -116,7 +119,27 @@ app.get("/campgrounds/:id", function (req, res) {
     }
   });
   });
+ //==================================
+  //  AUTH ROUTES
+  //==================================
 
+  //show register form
+  app.get("/register", function(req,res){
+    res.render("register");
+  });
+  //handle sign up logic
+  app.post("/register", function(req,res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+      if(err){
+        console.log(err);
+        return res.render("register");
+      }
+      passport.authenticate("local")(req,res,function(){
+        res.redirect("/campgrounds");
+      });
+    });
+  });
 
   //==================================
 
